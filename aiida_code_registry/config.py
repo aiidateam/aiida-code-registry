ca234bb79e51ca05eb7f292f9a82caa543835fd7
Leopold Talirz <leopold.talirz@gmail.com>
2021-01-26 15:42:41 +0100

    add jinja templating
    rough draft of how jinja templating could work.
    
    missing: parsing of default values (perhaps not as easy as I thought)

---
+++ aiida_code_registry/config.py
@@ -0,0 +1,6 @@
+"""
+AiiDA code registry
+"""
+import pathlib
+REGISTRY_URL = "https://raw.githubusercontent.com/aiidateam/aiida-code-registry/master"
+REGISTRY_PATH = pathlib.Path(__file__).resolve().parent.parent
