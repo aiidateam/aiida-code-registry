31a22390a66c73bdefb3a6880a918fc48ed9a620
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-10-13 18:25:28 +0200

    Adapt generate_json.py to the new folder structure

--- .make_ghpages/generate_json.py
+++ .make_ghpages/generate_json.py
@@ -15,20 +15,37 @@ YAML_SUFFIX = '.yaml'
 # Get all the available domains.
 final_dict = {dmn:{} for dmn in os.listdir(library_path) if os.path.isdir(library_path/dmn) and not dmn.startswith('.')}
 
+def parse_yaml_files_in_folder(folder_path, exclude=None):
+    if not os.path.isdir(folder_path):
+        raise TypeError(f"{folder_path} must point to an existing folder.")
+
+    result = {}
+
+    if exclude:
+        file_list = [ fname for fname in os.listdir(folder_path) if fname not in exclude ]
+    else:
+        file_list = os.listdir(folder_path)
+    
+    for fname in file_list:
+        with open(folder_path/fname) as yaml_file:
+            if fname.endswith(YAML_SUFFIX):
+                fname = fname[:-len(YAML_SUFFIX)]
+            else:
+                raise ValueError(f"The file {fname} has unsupported extension. Please use '{YAML_SUFFIX}'")
+            
+            result[fname] = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+    return result 
+
+
 # Loop over the available domains and extract the computes available under them.
 for domain in final_dict:
-    final_dict[domain] = {cmp:{} for cmp in os.listdir(library_path/domain) if os.path.isdir(library_path/domain/cmp) and not cmp == 'default'}
+    final_dict[domain] = {cmp:{} for cmp in os.listdir(library_path/domain) if os.path.isdir(library_path/domain/cmp) and not cmp in ['default', 'codes']}
 
     # Loop over the defined computers, and extract their setup and setup codes defined on them.
     for computer in final_dict[domain]:
-
-        for configuration in [ conf for conf in os.listdir(library_path/domain/computer) if conf != "README"]:
-            with open(library_path/domain/computer/configuration) as yaml_file:
-                if configuration.endswith(YAML_SUFFIX):
-                    configuration = configuration[:-len(YAML_SUFFIX)]
-                else:
-                    raise ValueError(f"The file {configuration} has unsupported extension. Please use '{YAML_SUFFIX}'")
-                final_dict[domain][computer][configuration] = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        final_dict[domain][computer] = parse_yaml_files_in_folder(library_path/domain/computer, exclude=["README"])
+        final_dict[domain][computer].update(parse_yaml_files_in_folder(library_path/domain/"codes"))
 
     # Extract the default computer.
     link = os.readlink(library_path/domain/'default')
